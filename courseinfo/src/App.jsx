602d3b2372f9b020edfaf8389c48687cf0c49d10
Dunia Goncalves <dgoncalves@everquote.com>
2023-12-20 20:29:07 -0500

    refactor to use components

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,5 +1,12 @@
-const App = () => {
-  const course = 'Half Stack application development'
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+  return  <p>{props.part} {props.exercise}</p>
+}
+
+const Content = (props) => {
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -8,18 +15,30 @@ const App = () => {
   const exercises3 = 14
 
   return (
+    <>
+      <Part part={part1} exercise={exercises1}/>
+      <Part part={part2} exercise={exercises2}/>
+      <Part part={part3} exercise={exercises3}/>
+    </>
+  )
+}
+
+//refactor this dry
+const Total = () => {
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content  />
+      <Total />
     </div>
   )
 }
