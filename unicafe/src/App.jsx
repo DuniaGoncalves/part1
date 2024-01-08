22f4b3227694086bcf63edb0cd962c9c6944e6a6
Dunia Goncalves <dgoncalves@everquote.com>
2024-01-07 23:53:38 -0500

    add a total state

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -5,17 +5,26 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
 
   const handleGoodClick = () => {
-    setGood(good + 1);
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    console.log(updatedGood)
+    setTotal(updatedGood + neutral + bad)
   }
 
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1);
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setTotal(good + updatedNeutral + bad)
   }
 
   const handleBadClick = () => {
-    setBad(bad + 1);
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setTotal(good + neutral + updatedBad)
   }
 
   return (
@@ -28,6 +37,7 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad} </p>
+      <p>all {total}</p>
     </div>
   )
 }
