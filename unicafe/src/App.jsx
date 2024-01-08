72398af82ef7c8aadd80848fc5d39a0f111c6e0b
Dunia Goncalves <dgoncalves@everquote.com>
2024-01-07 23:37:39 -0500

    create an app with 3 buttons that collects feedback

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -6,9 +6,28 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  }
+
   return (
     <div>
-      code here
+      <h2>give feedback</h2>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad} </p>
     </div>
   )
 }
